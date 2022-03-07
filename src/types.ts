dc44ec2d6f974585af934f9750aa44a8dcfea463
TanninOne <seppleviathan@gmx.de>
2022-03-07 15:40:47 +0100

    updated typings

--- src/types.ts
+++ src/types.ts
@@ -654,6 +654,7 @@ export interface ICollection extends ITimestamped {
   visible: boolean;
   description: string;
   summary: string;
+  commentLink: string;
 }
 
 export interface IExternalResource {
