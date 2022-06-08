f3ce118cc4f34ca5f92543513ebdf19bf7578125
TanninOne <seppleviathan@gmx.de>
2022-06-08 09:57:43 +0200

    updated build artifacts

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,4 @@
 export * from './types';
-export { NexusError, ParameterInvalid, RateLimitError, TimeoutError } from './customErrors';
+export { NexusError, ParameterInvalid, ProtocolError, RateLimitError, TimeoutError } from './customErrors';
 import Nexus from './Nexus';
 export default Nexus;
