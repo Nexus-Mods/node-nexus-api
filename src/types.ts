eb0404571c81f1ce8802da4bca05a38c6a62c93d
IDCs <adrianraer@gmail.com>
2025-06-12 18:23:56 +0100

    adding viewerIsBlocked property

--- src/types.ts
+++ src/types.ts
@@ -667,6 +667,7 @@ export interface ICollection extends ITimestamped {
   tileImage?: ICollectionImage;
   user: IGraphUser;
   userId: number;
+  viewerIsBlocked: boolean;
   visible: boolean;
   description: string;
   summary: string;
