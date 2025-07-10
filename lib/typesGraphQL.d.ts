70acf2d051ed438d6badfd84f68ba52e97de013c
thakyZ <950594+thakyZ@users.noreply.github.com>
2025-07-10 13:56:46 -0700

    update: fix issue with various vulnerabilities. - upgrade jsonwebtoken to 9.0.0:     fixes:       - moderate: <https://www.npmjs.com/advisories/1097694>       - moderate: <https://www.npmjs.com/advisories/1102458>       - high: <https://www.npmjs.com/advisories/1097690> - remove request:     it is unsupported now     fixes:       - moderate: <https://www.npmjs.com/advisories/1096727>       - remove tough-cookie:           fixes:             - moderate: <https://www.npmjs.com/advisories/1097682>

--- lib/typesGraphQL.d.ts
+++ lib/typesGraphQL.d.ts
@@ -1,28 +1,28 @@
 import { ICreateCollectionResult } from '.';
 import { ICollection, IDateTime, IFileHash, IGraphUser, IMod, IModFile, IRevision } from './types';
-export declare type PODs = number | string | boolean | IDateTime;
+export type PODs = number | string | boolean | IDateTime;
 export interface IFilter {
     [key: string]: any;
 }
-export declare type ValuesOf<T extends any[]> = T[number];
-export declare type Boolify<T> = T extends PODs ? boolean : T extends any[] ? Querify<ValuesOf<T>> : Querify<T>;
-export declare type QuerifyImpl<T> = {
+export type ValuesOf<T extends any[]> = T[number];
+export type Boolify<T> = T extends PODs ? boolean : T extends any[] ? Querify<ValuesOf<T>> : Querify<T>;
+export type QuerifyImpl<T> = {
     [P in keyof T]?: Boolify<T[P]>;
 };
-export declare type Querify<T> = QuerifyImpl<T> & {
+export type Querify<T> = QuerifyImpl<T> & {
     $filter?: IFilter;
 };
-export declare type GraphQLType = 'Int' | 'Float' | 'String' | 'Boolean' | 'DateTime' | string;
+export type GraphQLType = 'Int' | 'Float' | 'String' | 'Boolean' | 'DateTime' | string;
 export interface GraphQueryParameters {
     [key: string]: {
         type: GraphQLType;
         optional: boolean;
     };
 }
-export declare type ICollectionQuery = Querify<ICollection>;
-export declare type IRevisionQuery = Querify<IRevision>;
-export declare type ICreateCollectionQuery = Querify<ICreateCollectionResult>;
-export declare type IModQuery = Querify<IMod>;
-export declare type IModFileQuery = Querify<IModFile>;
-export declare type IUserQuery = Querify<IGraphUser>;
-export declare type IFileHashQuery = Querify<IFileHash>;
+export type ICollectionQuery = Querify<ICollection>;
+export type IRevisionQuery = Querify<IRevision>;
+export type ICreateCollectionQuery = Querify<ICreateCollectionResult>;
+export type IModQuery = Querify<IMod>;
+export type IModFileQuery = Querify<IModFile>;
+export type IUserQuery = Querify<IGraphUser>;
+export type IFileHashQuery = Querify<IFileHash>;
